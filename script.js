8b5413d4cde817441740ee79caacf592c78c9ca7
Nate Trichanh <natetrichanh@gmail.com>
2021-11-16 17:32:06 -0800

    initial commit

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-function swap () {
-    var h1 = document.querySelector("#h1");
+var h1 = document.querySelector("#h1");
+var conreq = document.querySelector("#conreq");
+var mycon = document.querySelector("#mycon");
+
+function accept(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    conreq.innerText--;
+    mycon.innerText++;
+}
+
+function removeuser(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    conreq.innerText--;
+}
+
+function swap() {
     h1.innerText = "Derrick Le";
-}
-
-function removeuser () {
-    var user = document.querySelector("#user1");
-    var num = document.querySelector("#conreq").innerText;
-    user.remove();
-    num = parseInt(num) - 1;
-    document.querySelector("#conreq").innerText = num;
-}
-
-function removeuser2 () {
-    var user = document.querySelector("#user2");
-    var num = document.querySelector("#conreq").innerText;
-    user.remove();
-    num = parseInt(num) - 1;
-    document.querySelector("#conreq").innerText = num;
-}
-
-function accept () {
-    var num = document.querySelector("#mycon").innerText;
-    num = parseInt(num) + 1;
-    document.querySelector("#mycon").innerText = num;
 }
